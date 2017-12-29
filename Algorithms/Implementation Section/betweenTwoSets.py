6af93078cac285a353231b3cc3d08657f2531a2f
Abhishek Karande <akarande70@gmail.com>
2017-12-29 20:35:49 +0530

    commit

---
+++ Algorithms/Implementation Section/betweenTwoSets.py
@@ -0,0 +1,13 @@
+import sys
+
+def getTotalX(a, b):
+    # Complete this function
+    mini=min(b)
+
+if __name__ == "__main__":
+    n, m = input().strip().split(' ')
+    n, m = [int(n), int(m)]
+    a = list(map(int, input().strip().split(' ')))
+    b = list(map(int, input().strip().split(' ')))
+    total = getTotalX(a, b)
+    print(total)
